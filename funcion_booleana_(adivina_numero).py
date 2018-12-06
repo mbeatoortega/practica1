1041cf0a9ac91b1492719153c289084bcaa84fb9
Miguel <mbeatoortga>
2018-12-06 19:00:04 -0400

    FUNCION BOOLEANA (ADIVINAR NUMERO)

--- funcion_booleana_(adivina_numero).py
+++ funcion_booleana_(adivina_numero).py
@@ -1,3 +1,5 @@
+print("Bienvenidos a adivina el numero")
+print("Es sencillo consiste en adivinar el numero ganador en 4 intentos ")
 numero_ganador = 8
 
 numero_usuario = int (input("adivina el numero ganador:"))
@@ -6,19 +8,21 @@ if numero_ganador == numero_usuario:
     print("!felicidades has Ganado!")
 
 else:
-
+    print("te quedan 3 intentos")
     numero_usuario = int(input("intenta otra vez "))
 
     if numero_ganador == numero_usuario:
         print("!felicidades has Ganado!")
 
     else:
+        print("te quedan 2 intentos")
         numero_usuario = int(input("intenta otra vez "))
 
         if numero_ganador == numero_usuario:
             print("!felicidades has Ganado!")
 
         else:
+            print("te quedan 1 intentos")
             numero_usuario = int(input("intenta otra vez "))
 
             if numero_ganador == numero_usuario:
